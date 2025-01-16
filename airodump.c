1fa4d9ac5d81616912cbbcee1f3cc44b592ced39
Gloway17 <ictdhn@gmail.com>
2025-01-16 12:22:03 -0500

    fix error

--- airodump.c
+++ airodump.c
@@ -61,6 +61,7 @@ uint8_t ap_count = 0;
 
 uint8_t current_channel = 1; // 현재 채널
 char *interface;         // 무선 인터페이스 이름
+time_t start_time;
 
 void channel_hopper() {
     static int group_offset = 0; // 그룹 내 오프셋
@@ -105,15 +106,28 @@ void parse_tagged_parameters(uint8_t *tagged_parameters, uint16_t length) {
             case 0x00: // ESSID
                 memcpy(PacketInfo.ESSID, &tagged_parameters[offset + 2], tag_len);
                 PacketInfo.ESSID[tag_len] = '\0'; // Null-terminate
-                printf("%s\n", PacketInfo.ESSID);
+                // printf("%s\n", PacketInfo.ESSID);
                 break;
 
             case 0x03: // Channel (CH)
                 PacketInfo.CH = tagged_parameters[offset + 2];
-                printf("%d\n", PacketInfo.CH);
+                // printf("%d\n", PacketInfo.CH);
                 break;
 
             case 0x01: // Supported Rates (MB - Maximum Bitrate)
+                if (tag_len > 0) {
+                    uint8_t max_rate = 0;
+                    for (uint8_t i = 0; i < tag_len; i++) {
+                        if (tagged_parameters[offset + 2 + i] > max_rate) {
+                            max_rate = tagged_parameters[offset + 2 + i];
+                        }
+                    }
+                    if (max_rate / 2 > PacketInfo.MB)
+                        PacketInfo.MB = max_rate / 2; // Convert to Mbps
+                }
+                break;
+            
+            case 0x32: // Supported Rates (MB - Maximum Bitrate)
                 if (tag_len > 0) {
                     uint8_t max_rate = 0;
                     for (uint8_t i = 0; i < tag_len; i++) {
@@ -131,12 +145,13 @@ void parse_tagged_parameters(uint8_t *tagged_parameters, uint16_t length) {
                     PacketInfo.ENC = 2; // Assuming WPA2
 
                     // Extract cipher suite (CIPHER)
-                    uint8_t cipher_offset = offset + 8; // Pairwise Cipher Suite starts at offset 8
+                    uint8_t cipher_offset = offset + 1 + 1 + 2 + 4 + 2; // Pairwise Cipher Suite starts at offset 8
                     if (cipher_offset < length) {
-                        uint32_t cipher_suite = tagged_parameters[cipher_offset];
-                        if (cipher_suite == 0x000FAC04) {
+                        uint8_t PCS_Type = tagged_parameters[cipher_offset + 3];
+                        // printf("%02x\n", PCS_Type);
+                        if (PCS_Type == 0x04) {
                             PacketInfo.CIPHER = 1; // CCMP (AES)
-                        } else if (cipher_suite == 0x000FAC02) {
+                        } else if (PCS_Type == 0x02) {
                             PacketInfo.CIPHER = 2; // TKIP
                         } else {
                             PacketInfo.CIPHER = 0; // Unknown
@@ -144,22 +159,24 @@ void parse_tagged_parameters(uint8_t *tagged_parameters, uint16_t length) {
                     }
 
                     // Extract authentication type (AUTH)
-                    uint8_t auth_offset = offset + 14; // AKM Suite starts at offset 14
+                    uint8_t auth_offset = cipher_offset + 4 + 2; // AKM Suite starts at offset 14
                     if (auth_offset < length) {
-                        uint32_t auth_suite = tagged_parameters[auth_offset];
-                        if (auth_suite == 0x000FAC02) {
+                        uint8_t AKM_Type = tagged_parameters[auth_offset + 3];
+                        // printf("%02x\n", AKM_Type);
+                        if (AKM_Type == 0x02) {
                             PacketInfo.AUTH = 1; // PSK
-                        } else if (auth_suite == 0x000FAC01) {
+                        } else if (AKM_Type == 0x01) {
                             PacketInfo.AUTH = 2; // EAP
                         } else {
                             PacketInfo.AUTH = 0; // Unknown
                         }
                     }
+                    printf("%d %d %d\n", PacketInfo.ENC, PacketInfo.CIPHER, PacketInfo.AUTH);
                 }
                 break;
             
             default:
-                printf("Unknown Tag Number: %u, Length: %u\n", tag_num, tag_len);
+                // printf("Unknown Tag Number: %u, Length: %u\n", tag_num, tag_len);
                 break;
         }
 
@@ -192,28 +209,45 @@ int airodump_print(uint8_t ch, struct packet_info *PacketInfo) {
     // move curser (1, 1)
     printf("\033[%d;%dH", 1, 1);
 
-    printf("[ CH %2d ][ Elapsed:  0 s ][ %04d-%02d-%02d %02d:%02d:%02d ]\n\n", 
-            ch, 
-            // 시간 출력: YYYY-MM-DD HH:MM 형식
-            local->tm_year + 1900, // 연도는 1900을 더해야 실제 연도가 됨
-            local->tm_mon + 1,    // 월은 0부터 시작하므로 1을 더해야 함
-            local->tm_mday,       // 일
-            local->tm_hour,       // 시
-            local->tm_min,
-            local->tm_sec);
+    time_t end_time = time(NULL);
+    int8_t cal_time = (int8_t)(end_time - start_time);
+
+    if (cal_time > 60) {
+        printf("[ CH %2d ][ Elapsed: %d mins ][ %04d-%02d-%02d %02d:%02d:%02d ]\n\n", 
+        ch,
+        (int8_t)(cal_time / 60),
+        // 시간 출력: YYYY-MM-DD HH:MM 형식
+        local->tm_year + 1900, // 연도는 1900을 더해야 실제 연도가 됨
+        local->tm_mon + 1,    // 월은 0부터 시작하므로 1을 더해야 함
+        local->tm_mday,       // 일
+        local->tm_hour,       // 시
+        local->tm_min,
+        local->tm_sec);
+    } else {
+        printf("[ CH %2d ][ Elapsed: %d s ][ %04d-%02d-%02d %02d:%02d:%02d ]\n\n", 
+        ch,
+        cal_time,
+        // 시간 출력: YYYY-MM-DD HH:MM 형식
+        local->tm_year + 1900, // 연도는 1900을 더해야 실제 연도가 됨
+        local->tm_mon + 1,    // 월은 0부터 시작하므로 1을 더해야 함
+        local->tm_mday,       // 일
+        local->tm_hour,       // 시
+        local->tm_min,
+        local->tm_sec);
+    }
     
     printf(" BSSID              PWR  Beacons    #Data, #/s  CH   MB   ENC CIPHER  AUTH ESSID\n\n");
 
-    for (int i = 0; i < ap_count; i++) {
-        printf(" %02x:%02x:%02x:%02x:%02x:%02x  %d  %7d    %5d  %3d  %2d  %3d   %s %s   %s %s\n",
+    for (int i = ap_count - 1; i >= 0; i--) {
+        printf(" %02x:%02x:%02x:%02x:%02x:%02x  %d  %7d    %5d  %3d  %2d  %3d   %-4s %-4s   %-4s %s\n",
             aps[i].BSSID[0], aps[i].BSSID[1], aps[i].BSSID[2], aps[i].BSSID[3], aps[i].BSSID[4], aps[i].BSSID[5],
             aps[i].PWR,     // PWR
             aps[i].Beacons, // Beacons
             0,              // #Data,
             0,              // #/s
             aps[i].CH,      // CH
-            0,              // MB
-            aps[i].ENC    == 1 ? "WPA2" : "????", // ENC
+            aps[i].MB,              // MB
+            aps[i].ENC    == 2 ? "WPA2" : "????", // ENC
             aps[i].CIPHER == 1 ? "AES" : (aps[i].CIPHER == 2 ? "TKIP" : "????"), // CIPHER
             aps[i].AUTH   == 1 ? "PSK" : (aps[i].AUTH   == 2 ? "EAP" : "????"), // AUTH
             aps[i].ESSID);  // ESSID
@@ -227,6 +261,8 @@ int main(int argc, char *argv[]) {
     struct pcap_pkthdr *header;
     const u_char *packet;
     int res;
+
+    start_time = time(NULL);
 
     // Check if device name is provided
     if (argc < 2) {
